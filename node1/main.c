ad75aa4a025be6acc16619ee58665582d9638a4e
aslakmyhre <aslakmyh@stud.ntnu.no>
2026-09-09 13:52:12 +0200

    independent LED toggle

--- node1/main.c
+++ node1/main.c
@@ -4,20 +4,17 @@
 #include <stdio.h>
 #include "uart.h"
 
+#define LED_ON_MS 300
 
 int main(void) {
     DDRA = 0xFF; //PA0-PA7 as outputs
     uart_init(UBRR_VALUE(9600));
 
-    unsigned n = 0;
-
     while (1) {
-        PORTA ^= 0xFF;
-
-        _delay_ms(500); //blinking
-
-        putchar(getchar()); //mirror input
-
-        //printf("blink %u\n", n++); //counter
+        for (uint8_t pin = 0; pin < 8; pin++) {
+            PORTA = (1 << pin); //only this pin high
+            printf("PA%u\n", pin);
+            _delay_ms(LED_ON_MS);
+        }
     }
 }
