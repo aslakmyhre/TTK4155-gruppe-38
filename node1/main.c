81053cbf815001af6f781edfa2047aafb58dd4b6
aslakmyhre <aslakmyh@stud.ntnu.no>
2026-09-09 13:44:20 +0200

    activate PA0 to PA7

--- node1/main.c
+++ node1/main.c
@@ -6,13 +6,13 @@
 
 
 int main(void) {
-    DDRA |= (1<<PA0);
+    DDRA = 0xFF; //PA0-PA7 as outputs
     uart_init(UBRR_VALUE(9600));
 
     unsigned n = 0;
 
     while (1) {
-        PORTA ^= (1<<PA0);
+        PORTA ^= 0xFF;
 
         _delay_ms(500); //blinking
 
